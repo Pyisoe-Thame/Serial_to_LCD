cdaf27bd26bcc8a2f912cb9c4e037ff4938a3816
Pyi_Soe_Thame <pyisoethame@gmail.com>
2024-02-23 19:45:34 +0630

    optimised for all rows and columns

--- Serial_to_LCD.ino
+++ Serial_to_LCD.ino
@@ -1,7 +1,12 @@
+/*
+  Author : Pyae Sone Hein
+  Date   : Feb/23/2024
+*/
+
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
-#define interval 170
+#define interval 200
 
 // Define the LCD dimensions
 const uint8_t numRows = 2;
@@ -13,9 +18,11 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 String message = "";
 
-void LCD_Print_Msg( String &str);
-void rmSpaceatColumn0(String &str);
-void removeThis(String &str, int index);
+void LCD_ShiftTextOnLimit( String &str);
+void rmUnwantedSpace(String &str);
+void rmIndex(String &str, int index);
+void shiftUp(String &str, int shiftTime, bool leaveBotLine);
+void printStringOnRow( String &str, int row, int delayTime);
 
 void setup() 
 {
@@ -27,73 +34,63 @@ void setup()
   
 }
 
-void loop() {
+void loop() 
+{
   // Sample text to display
   if( Serial.available() > 0)
   {
     message = Serial.readStringUntil('\n');
-    LCD_Print_Msg(message);
+    LCD_ShiftTextOnLimit(message);
     Serial.println(message);
+    delay(10000);
+    lcd.clear();
   }
 }
 
-void LCD_Print_Msg( String str)
+void LCD_ShiftTextOnLimit( String &str)
 {
-  String tempStr = str;
-  rmSpaceatColumn0(tempStr);  // prepare the string suitable for 16 column LCD
+  String tempStr = str;  // just make a copy to save string integrity
+  rmUnwantedSpace(tempStr);  // prepare the string suitable for 16 column LCD
   lcd.clear();
-  uint8_t len = tempStr.length() - 1;  // minus one to leave out the '\0' (we are not printing the null char)
-  uint8_t pos = 0;
-  uint8_t overhead = 0;
-  while( pos < numCols && pos < len)  // the first row
+/*
+  length() doesn't include counting '\0'
+  change the type of len if size of int is not big enough to hold value
+  numCols - 1 added to include counting the last substring
+  because the last substring could have become modulation and not get printed
+*/
+  int numSubStr = ( tempStr.length() + ( numCols - 1)) / numCols;
+  
+  for( int i = 0; i < numRows; i++)  // until it reaches the readable row limit
   {
-    lcd.setCursor(0, 0);
-    for( int i = 0; i <= pos; i++)
-      lcd.print( tempStr[i]);
-    delay(interval);
-    pos++;
+    String substring = tempStr.substring(i * numCols, (i + 1) * numCols);
+    Serial.println(substring);
+    printStringOnRow( substring, i, interval);
   }
-  while( len > numCols)  // if more words remained to print
+  if( numSubStr > numRows)
   {
-    len -= numCols;
-    overhead += numCols;
-    pos = 0;
-    while( pos < numCols && pos < len)  // do the second row
+    int shiftTime = 1;  // shifting has to begin
+    for( int i = numRows; i < numSubStr; i++) 
     {
-      lcd.setCursor( 0, 1);
-      for( int i = 0; i <= pos; i++)
-        lcd.print( tempStr[ i + overhead]);
-      delay(interval);
-      pos++;
-    }
-    if( len > numCols)
-    {
-      lcd.clear();
-      pos = 0;
-      while( pos < numCols && pos < len)
-      {
-        lcd.setCursor( 0, 0);  // shift the former second line
-        for( int i = 0; i <= pos; i++)
-          lcd.print( tempStr[ i + overhead]);
-        pos++;
-      }
+      delay(interval);  // put interval also before shifting
+      shiftUp( tempStr, shiftTime, true);  // start shifting
+      String substring = tempStr.substring(i * numCols, (i + 1) * numCols);
+      Serial.println(substring);
+      printStringOnRow( substring, numRows-1, interval);
+      shiftTime++;  // waiting for the next shifting
     }
   }
-  // delay(5000);
-  // lcd.clear();
 }
 
-void rmSpaceatColumn0(String &str) 
+void rmUnwantedSpace(String &str) 
 {
-  int length = str.length();
-  bool removedSpace = false;
+  int length = str.length();  
 
   checkAgain:  // check again if string is changed (inefficient for long string)
   for( int i = 0; i < length; i += numCols)  // for all char where index % 16 == 0
   {
     if( str[i] == ' ')  // check if the char at 0 column is a space
     {  
-      removeThis( str, i);  // Remove the space character
+      rmIndex( str, i);  // Remove the space character
       length--;
       goto checkAgain;
     }
@@ -101,7 +98,7 @@ void rmSpaceatColumn0(String &str)
   return ;
 }
 
-void removeThis(String &str, int index) 
+void rmIndex(String &str, int index) 
 {
   do
   {
@@ -111,3 +108,35 @@ void removeThis(String &str, int index)
   str.remove( str.length() - 1);  // resize the string (reduce by 1 char)
 }
 
+void shiftUp(String &str, int shiftTime, bool leaveBotLine)
+{
+  lcd.clear();
+  if( leaveBotLine == false)  // check if request to leave the bottom row for some printing performance
+  {
+    for( int row = 0; row < numRows; row++)  // for all rows
+    {
+      lcd.setCursor( 0, row);  // change line
+      for( int i = 0; i < numCols; i++)
+        lcd.print( str[ i + ( (row + shiftTime) * numCols)]);  // print depending on row and shift time on each column
+    }
+  }
+  else  // need to leave the bottom row (not getting printed by this function)
+  {
+    for( int row = 0; row < numRows-1; row++)  // just up to the row before the last
+    {
+      lcd.setCursor( 0, row);  // change line
+      for( int i = 0; i < numCols; i++)  
+        lcd.print( str[ i + ( (row + shiftTime) * numCols)]);  // print depending on row and shift time on each column
+    }
+  }
+}
+
+void printStringOnRow( String &str, int row, int delayTime) 
+{
+  lcd.setCursor( 0, row); // Set cursor to the beginning of the specified row
+  for( int i = 0; i < str.length(); i++) 
+  {
+    lcd.print(str[i]); // Print the current character
+    delay(delayTime);  // Wait for a short time
+  }
+}
